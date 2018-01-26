f52991b0a70b9107353bcf251309226533e5adf4
BeetleChunks <BeetleChunks@users.noreply.github.com>
2018-01-25 18:36:50 -0600

    Update bitcoin_util.py

--- bitcoin_util.py
+++ bitcoin_util.py
@@ -1,41 +1,117 @@
+import requests
 import hashlib
 import binascii
 import struct
 import base58
+import time
 import sys
+import timeit
 
 from ecdsa import SigningKey,SECP256k1
 from ecdsa.util import PRNG
 from optparse import OptionParser
 
 class Bitcoin:
-	def __init__(self, curve=SECP256k1, seed=None, version=None):
-		self.EC_CURVE   = curve
+	def __init__(self, curve=SECP256k1, seed=None, version=None, privateHexKey=None, 
+		wif=False, testnet=False, address=None):
+		
+		if address == None:
+			self.EC_CURVE   = curve
+			self.TESTNET = testnet
 
-		if seed == None:
-			self.EC_SEED = 'DEADBEEF'
+			if seed == None:
+				self.EC_SEED = 'DEADBEEF'
+			else:
+				self.EC_SEED = seed
+
+			if version == None:
+				self.BT_VERSION = 0x00
+			else:
+				self.BT_VERSION = version
+
+			self.BT_ADDRESS = self.init_get_bitcoin_address(privateHexKey=privateHexKey, wif=wif)
+
 		else:
-			self.EC_SEED = seed
+			self.BT_ADDRESS = address
 
-		if version == None:
-			self.BT_VERSION = 0x00
+	def init_get_bitcoin_address(self, privateHexKey=None, wif=False):
+		# This occurs if you do not specify a private key with the --privatekey flag
+		# generating a private key with default "DEADBEEF" seed, unless --seed is specified
+		if privateHexKey == None:
+			self.BT_PRIV_KEY = self.get_private_ECDSA_key()
+
+			self.BT_PRIV_WIF_KEY = self.get_wif_from_private_hex_key(binascii.hexlify(self.BT_PRIV_KEY))
+			self.BT_PRIV_HEX_KEY = binascii.hexlify(self.BT_PRIV_KEY).upper()
+
+		# This occurs if you specify a private hex key from the command line with the --privatekey
+		# flag and also tac on the --wif flag, indicating the private key supplied is in WIF format
+		elif wif == True:
+			self.BT_PRIV_KEY = self.get_private_key_from_wif(privateHexKey)
+
+			self.BT_PRIV_WIF_KEY = privateHexKey
+			self.BT_PRIV_HEX_KEY = binascii.hexlify(self.BT_PRIV_KEY).upper()
+
+		# This occurs if you specify a private hex key from the command line with the --privatekey
+		# and do NOT supply the --wif flag, indicating the private key supplied is standard hex format
 		else:
-			self.BT_VERSION = version
+			self.BT_PRIV_KEY = binascii.unhexlify(privateHexKey)
 
-		self.BT_ADDRESS = self.init_get_bitcoin_address()
+			self.BT_PRIV_WIF_KEY = self.get_wif_from_private_hex_key(privateHexKey)
+			self.BT_PRIV_HEX_KEY = binascii.hexlify(self.BT_PRIV_KEY).upper()
 
-	def init_get_bitcoin_address(self):
-		privateKey   = self.get_private_ECDSA_key()
-		publicKey    = self.get_public_ECDSA_key(privateKey)
-		pubSHA256    = self.get_sha256_hash(publicKey)
-		pubRIPEMD160 = self.get_ripemd160_hash(pubSHA256)
-		pubVersioned = self.get_versioned_key(pubRIPEMD160)
-		pub160SHA256 = self.get_sha256_hash(pubVersioned)
-		pubSHA256x2  = self.get_sha256_hash(pub160SHA256)
-		pubChecksum  = self.get_checksum_appended_hash(pubVersioned, pubSHA256x2)
+		publicKey        = self.get_public_ECDSA_key(self.BT_PRIV_KEY)
+		pubSHA256        = self.get_sha256_hash(publicKey)
+		pubRIPEMD160     = self.get_ripemd160_hash(pubSHA256)
+		pubVersioned     = self.get_versioned_key(pubRIPEMD160)
+		pub160SHA256     = self.get_sha256_hash(pubVersioned)
+		pubSHA256x2      = self.get_sha256_hash(pub160SHA256)
+		pubChecksum      = self.get_checksum_appended_hash(pubVersioned, pubSHA256x2)
 		
 		return self.get_base58check_string(pubChecksum)
+
+	def get_wif_from_private_hex_key(self, privateHexKey):
+		privateKey = binascii.unhexlify(privateHexKey)
+
+		# 2 - add 0x80 byte in front for mainnet and 0xef for testnet
+		if self.TESTNET == False:
+			privateKey = struct.pack('1B',*([0x80])) + privateKey
+		else:
+			privateKey = struct.pack('1B',*([0xef])) + privateKey
+
+		# 3 and 4 - perform SHA256 on the extended key and then again on result hash
+		privSHA256 = self.get_sha256_hash(self.get_sha256_hash(privateKey))
+
+		# 5 - use first 4 bytes of the result hash as the checksum
+		privChecksum = privSHA256[0:4]
+
+		# 6 - add 4 bytes from [5] to the end of the extended hash from [2]
+		checksum = []
+		for i in range(0, 4):
+			checksum.append(int(binascii.hexlify(privChecksum[i]), 16))
+
+		privCheckSumKey = privateKey + struct.pack('4B',*(checksum))
+
+		# 7 - convert the resultant key to the base58check encoding
+		wifPrivateB58Key = self.get_base58check_string(privCheckSumKey)
+
+		return wifPrivateB58Key
+
+	def get_private_key_from_wif(self, wifPrivateB58Key):
+		privateHexKey = base58.b58decode(wifPrivateB58Key)
+		return privateHexKey[1:-4]
 	
+	def get_wif_validated_checksum_status(self):
+		privCheckSumKey = base58.b58decode(self.BT_PRIV_WIF_KEY)
+		checksum        = privCheckSumKey[-4:]
+		privateKey      = privCheckSumKey[0:-4]
+		newChecksum     = self.get_sha256_hash(self.get_sha256_hash(privateKey))[0:4]
+
+		if checksum == newChecksum:
+			return True
+
+		else:
+			return False
+
 	# 0 - Generate and return raw private ECDSA key
 	def get_private_ECDSA_key(self):
 		return SigningKey.generate(curve=self.EC_CURVE, entropy=PRNG(self.EC_SEED)).to_string()
@@ -82,6 +158,18 @@ class Bitcoin:
 	def print_hex_key(self, rawKey):
 		print binascii.hexlify(rawKey).upper()
 
+	def check_address_first_seen(self):
+		URI = 'http://blockchain.info/q/addressfirstseen/' + self.BT_ADDRESS
+		response = requests.get(URI)
+		
+		return time.strftime("%a, %b %d, %Y %H:%M:%S %Z", time.localtime(int(response.text.encode('utf-8'), 0)))
+
+	def check_address_balance(self):
+		URI = 'http://blockchain.info/q/addressbalance/' + self.BT_ADDRESS
+		response = requests.get(URI)
+		
+		return response.text.encode('utf-8')
+
 def getCommandLineArgs(printHelp=False):
 	parser = OptionParser(add_help_option=False)
 
@@ -101,6 +189,48 @@ def getCommandLineArgs(printHelp=False):
 			dest="version")
 
 	parser.add_option(
+			"--address",
+			help="Specify a bitcoin address and get date first seen and balance information",
+			default=None,
+			action="store",
+			dest="address")
+
+	parser.add_option(
+			"--privatekey",
+			help="Specify the private key in hex format",
+			default=None,
+			action="store",
+			dest="privatekey")
+
+	parser.add_option(
+			"--wif",
+			help="Use if the supplied private key is in Wallit Import Format (WIF)",
+			default=False,
+			action="store_true",
+			dest="wif")
+
+	parser.add_option(
+			"--testnet",
+			help="Use to generate a testnet key instead of mainnet",
+			default=False,
+			action="store_true",
+			dest="testnet")
+
+	parser.add_option(
+			"--checkbalance",
+			help="Use to check bitcoin address balance against http://blockchain.info",
+			default=False,
+			action="store_true",
+			dest="checkbalance")
+
+	parser.add_option(
+			"--validate",
+			help="Used to validate the supplied or generated WIF key's checksum",
+			default=False,
+			action="store_true",
+			dest="validate")
+
+	parser.add_option(
 			"-h", "--help",
 			help="Display this help message",
 			default=False,
@@ -113,8 +243,13 @@ def getCommandLineArgs(printHelp=False):
 		parser.print_help()
 
 		print "\nExamples:\n"
-		print "bitcoin_util.py --seed Sup3rS3cr3tP4ss"
-		print "bitcoin_util.py --version 0x05 --seed Sup3rS3cr3tP4ss"
+		print "  bitcoin_util.py --seed Sup3rS3cr3tP4ss"
+		print "  bitcoin_util.py --version 0x05 --seed Sup3rS3cr3tP4ss"
+		print "  bitcoin_util.py --seed Sup3rS3cr3tP4ss --testnet"
+		print "  bitcoin_util.py --privatekey 4324D402...2B789323"
+		print "  bitcoin_util.py --privatekey 5JKrgU8m...6ksEfPdP --wif"
+		print "  bitcoin_util.py --privatekey 5JKrgU8m...6ksEfPdP --wif --validate"
+		print "  bitcoin_util.py --privatekey 4324D402...2B789323 --validate --checkbalance"
 		print "\n"
 
 		sys.exit(0)
@@ -124,8 +259,33 @@ def getCommandLineArgs(printHelp=False):
 def main():
 	(options, args) = getCommandLineArgs()
 
-	bc = Bitcoin(seed=options.seed, version=options.version)
-	print bc.BT_ADDRESS
+	bc = Bitcoin(seed=options.seed, version=options.version, privateHexKey=options.privatekey, 
+			wif=options.wif, testnet=options.testnet, address=options.address)
+
+	if options.address == None:
+		print "\n"
+		print "[+] BT Address      : " + bc.BT_ADDRESS
+		print "[+] BT WIF PRIV KEY : " + bc.BT_PRIV_WIF_KEY
+		print "[+] BT HEX PRIV KEY : " + bc.BT_PRIV_HEX_KEY
+
+	else:
+		print "\n"
+		print "[+] BT Address      : " + bc.BT_ADDRESS
+
+	if options.checkbalance == True or options.address != None:
+		print "[+] First seen      : " + bc.check_address_first_seen()
+		print "[+] Balance         : " + bc.check_address_balance()
+
+	if options.validate == True and options.address == None:
+		status = bc.get_wif_validated_checksum_status()
+
+		if status == True:
+			print "[+] Status          : Valid"
+
+		else:
+			print "[+] Status          : Invalid!"
+
+	print "\n"
 
 if __name__ == '__main__':
 	main()
